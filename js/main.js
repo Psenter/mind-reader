3231e2f6ba59baaacac93070e6e2dbd8af87f49e
Parker S <parker.senter@gmail.com>
2023-06-08 21:04:53 +0000

    Got JS buttons working and display changing.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,79 @@
-let webPage = document.getElementById('webPage');
-let instruct = document.getElementById('instruct');
-let nextBtn = document.getElementById('next');
-let example = document.getElementById('example');
+let webPage = document.getElementById("webPage");
+let instruct = document.getElementById("instruct");
+let nextBtn = document.getElementById("next");
+let example = document.getElementById("example");
 let goRef = document.getElementById("goRef");
 
-let pages = {
-    page1: [
-        instruct.textContent = 'I can read your mind.',
-        nextBtn.style.display = "none",
-        example.style.display = "none",
-        goRef.textContent= 'GO'
-    ],
-    page2: [
-        instruct.textContent = 'Pick a number from 01-99',
-        nextBtn.style.display = "block",
-        nextBtn.textContent = 'Next',
-        example.style.display = "block",
-        example.textContent = 'when you have a number click next',
-        goRef.textContent = img/Refresh_icon.svg.png
-    ],
-    page3: [
-        instruct.textContent = 'Add both digits together to get a new number',
-        nextBtn.textContent = 'Next',
-        example.textContent = 'Ex: 14 is 1 + 4 = 5\nclick next to proceed',
-        goRef.textContent = img/Refresh_icon.svg.png
-    ],
-    page4: [
-        instruct.textContent = 'Subtract your new number from the original number',
-        nextBtn.textContent = 'Next',
-        example.textContent = 'Ex: 14 - 5 = 9\nclick next to proceed',
-        goRef.textContent = img/Refresh_icon.svg.png
-    ],
-    page5: [
-        //fix 
-        instruct.textContent(),
-        nextBtn.textContent = 'Reveal',
-        example.textContent = 'Find your new number.\nNote the symbol beside the number',
-        goRef.textContent = img/Refresh_icon.svg.png
-    ],
-    page6: [
-        //fix
-        instruct.textContent(''),
-        nextBtn.style.display = "none",
-        example.textContent(''),
-        goRef.textContent = img/Refresh_icon.svg.png
-    ]
-};
-
-let symbols = [
-    '@',
-    '#',
-    '$',
-    '%',
-    '&'
+let pages = [
+  {
+    instruct: "I can read your mind",
+    nextBtn: false,
+    goRef: "Go"
+  },
+  {
+    instruct: "Pick a number from 01-99",
+    nextBtn: "Next",
+    example: "when you have your number click next",
+    goRef: "<img src='img/Refresh_icon.svg.png'>",
+  },
+  {
+    instruct: "Add both digits together to get a new number",
+    nextBtn: "Next",
+    example: "Ex; 14 is 1 + 4 = 5, click next to proceed",
+    goRef: "<img src='img/Refresh_icon.svg.png'>",
+  },
+  {
+    instruct: "Subtract your new number from the original number",
+    nextBtn: "Next",
+    example: "Ex; 14 - 5 = 9, click next to proceed",
+    goRef: "<img src='img/Refresh_icon.svg.png'>"
+  },
+  {
+    nextBtn: "Reveal",
+    example: "Find your new number. Note the symbol beside the number",
+    goRef: "<img src='img/Refresh_icon.svg.png'>"
+  },
+  {
+    instruct: "&",
+    example: 'Your symbol is: "&" ',
+    goRef: "<img src='img/Refresh_icon.svg.png'>",
+  }
 ];
 
-nextBtn.addEventListener('click', function(webPage) {
-    webPage = pages[0]++;
-    return webPage;
-});
+let symbols = ["@", "#", "$", "%", "&"];
+
+let currentPage = 0;
+
+function nextPage() {
+    currentPage++;
+    render();
+}
+
+function goBtn() {
+    if (currentPage === 0) {
+        nextPage();
+    }
+    else {
+        currentPage = 0;
+        render();
+    }
+}
+
+goRef.addEventListener('click', goBtn);
+
+nextBtn.addEventListener('click', nextPage);
+
+function render() {
+    instruct.textContent = pages[currentPage].instruct;
+    example.textContent = pages[currentPage].example;
+    goRef.innerHTML = pages[currentPage].goRef;
+    if (pages[currentPage].nextBtn === false) {
+        nextBtn.style.visibility = 'hidden';
+    }
+    else {
+        nextBtn.style.visibility = 'visible';
+        nextBtn.textContent = 'Next'
+    }
+}
+
+render();
