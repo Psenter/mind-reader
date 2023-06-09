e94e1388030156664871f3f2e2c1a0b670abce74
Parker S <parker.senter@gmail.com>
2023-06-09 13:59:13 +0000

    Added more to JS.

--- js/main.js
+++ js/main.js
@@ -16,30 +16,31 @@ let pages = [
     instruct: "Pick a number from 01-99",
     nextBtn: "Next",
     example: "when you have your number click next",
-    goRef: "<img src='img/Refresh_icon.svg.png'>"
+    goRef: "↻"
   },
   {
     instruct: "Add both digits together to get a new number",
     nextBtn: "Next",
     example: "Ex; 14 is 1 + 4 = 5, click next to proceed",
-    goRef: "<img src='img/Refresh_icon.svg.png'>",
+    goRef: "↻"
   },
   {
     instruct: "Subtract your new number from the original number",
     nextBtn: "Next",
     example: "Ex; 14 - 5 = 9, click next to proceed",
-    goRef: "<img src='img/Refresh_icon.svg.png'>"
+    goRef: "↻"
   },
   {
+    instruct: "",
     nextBtn: "Reveal",
     example: "Find your new number. Note the symbol beside the number",
-    goRef: "<img src='img/Refresh_icon.svg.png'>"
+    goRef: "↻"
   },
   {
-    instruct: "&",
+    instruct: "",
     nextBtn: false,
-    example: 'Your symbol is: "&" ',
-    goRef: "<img src='img/Refresh_icon.svg.png'>"
+    example: "",
+    goRef: "↻"
   }
 ];
 
@@ -74,7 +75,12 @@ nextBtn.addEventListener('click', nextPage);
 
 //renders the array onto the screen
 function render() {
-    instruct.textContent = pages[currentPage].instruct;
+    if (pages[currentPage].instruct === true) {
+      instruct.textContent = multiNine();
+    }
+    else {
+      instruct.textContent = pages[currentPage].instruct;
+    }
     example.textContent = pages[currentPage].example;
     goRef.innerHTML = pages[currentPage].goRef;
     //hides next button when needed to be hidden
@@ -85,6 +91,28 @@ function render() {
         nextBtn.style.visibility = 'visible';
         nextBtn.textContent = pages[currentPage].nextBtn;
     }
+}
+
+let chosenSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+//create string
+//concat nums and symbols into one string
+//return string
+function multiNine() {
+  var str = '';
+  chosenSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  for (let i = 0; i < 100; i++) {
+    if (i % 9 === 0) {
+      str = str + i + ' = ' + chosenSymbol + '</br>';
+    }
+    else {
+      const randomSymbol = Math.floor(Math.random() * symbols.length);
+      const item = symbols[randomSymbol];
+      str = str + i + ' = ' + item + '</br>';
+    }
+  }
+  pages[5].instruct = chosenSymbol;
+  pages[4].instruct = str;
 }
 
 //executes the render function
