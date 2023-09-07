529380864481ec95f7abbc46037de3ccaa561128
Parker S <111610335+Psenter@users.noreply.github.com>
2023-09-07 11:36:54 -0400

    Numbers have random symbols now, finished styling.

--- js/main.js
+++ js/main.js
@@ -5,38 +5,75 @@ let goReset = document.getElementById("goReset");
 let symbols = ["@", "#", "$", "%", "&", "*"];
 
 let state = {
-    pageIndex: 0,
-    pagesArr: [
-        { title: "I can read your mind", hideNextbtn: true, t1: "", goReset: "GO"},
+  pageIndex: 0,
+  pagesArr: [
+    { title: "I can read your mind", hideNextbtn: true, t1: "", goReset: "GO" },
 
-        { title: "Pick a number from 01-99", nextbtn: "NEXT", t1: "when you have your number click next", goReset: "↻"},
+    {
+      title: "Pick a number from 01-99",
+      nextbtn: "NEXT",
+      t1: "when you have your number click next",
+      goReset: "↻",
+    },
 
-        { title: "Add both digits together  to get a new number", nextbtn: "NEXT", t1: "Ex: 14 is 1 + 4 = 5 <br> click next to proceed", goReset: "↻"},
+    {
+      title: "Add both digits together  to get a new number",
+      nextbtn: "NEXT",
+      t1: "Ex: 14 is 1 + 4 = 5 <br> click next to proceed",
+      goReset: "↻",
+    },
 
-        { title: "Subtract your new number from the orginial number", nextbtn: "NEXT", t1: "Ex: 14 - 5 = 9 <br> click next to proceed", goReset: "↻"},
+    {
+      title: "Subtract your new number from the original number",
+      nextbtn: "NEXT",
+      t1: "Ex: 14 - 5 = 9 <br> click next to proceed",
+      goReset: "↻",
+    },
 
-        { title: "", nextbtn: "REVEAL", t1: "Find your new number <br> Note the symbol beside the number", goReset: "↻"},
+    {
+      title: "",
+      nextbtn: "REVEAL",
+      t1: "Find your new number <br> Note the symbol beside the number",
+      goReset: "↻",
+    },
 
-        { title:"&", hideNextbtn: true, t1: "Your symbol is: <br> &", goReset: "↻"}
-    ]
+    { title: "$", hideNextbtn: true, t1: "Your symbol is: <br> $", goReset: "↻" },
+  ],
 };
 
 function init() {
-    title.innerHTML = state.pagesArr[state.pageIndex].title;
-    nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
-    t1.innerHTML = state.pagesArr[state.pageIndex].t1;
-    goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
+  title.innerHTML = state.pagesArr[state.pageIndex].title;
+  nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
+  t1.innerHTML = state.pagesArr[state.pageIndex].t1;
+  goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
 
-    if (state.pagesArr[state.pageIndex].hideNextbtn) {
-        nextbtn.style.display = "none";
-    }
-    else {
-        nextbtn.style.display = "block";
-    }
+  if (state.pagesArr[state.pageIndex].hideNextbtn) {
+    nextbtn.style.display = "none";
+  } else {
+    nextbtn.style.display = "block";
+  }
 }
 
 function nextPage() {
-    state.pageIndex = state.pageIndex + 1
+  state.pageIndex = state.pageIndex + 1;
+
+  title.innerHTML = state.pagesArr[state.pageIndex].title;
+  nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
+  t1.innerHTML = state.pagesArr[state.pageIndex].t1;
+  goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
+
+  if (state.pagesArr[state.pageIndex].hideNextbtn) {
+    nextbtn.style.display = "none";
+  } else {
+    nextbtn.style.display = "block";
+  }
+}
+
+function resetPage() {
+  if (goReset.innerHTML === "GO") {
+    nextPage();
+  } else if (goReset.innerHTML === "↻") {
+    state.pageIndex = 0;
 
     title.innerHTML = state.pagesArr[state.pageIndex].title;
     nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
@@ -44,43 +81,39 @@ function nextPage() {
     goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
 
     if (state.pagesArr[state.pageIndex].hideNextbtn) {
-        nextbtn.style.display = "none";
+      nextbtn.style.display = "none";
+    } else {
+      nextbtn.style.display = "block";
     }
-    else {
-        nextbtn.style.display = "block";
-    }
+  }
 }
 
-function resetPage() {
-    if (goReset.innerHTML === "GO") {
-        nextPage();
-    }
-    else if (goReset.innerHTML === "↻"){
-        state.pageIndex = 0
+function generateNumbersWithSymbols() {
+  const numbersWithSymbols = [];
+  const symbolMapping = {};
 
-        title.innerHTML = state.pagesArr[state.pageIndex].title;
-        nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
-        t1.innerHTML = state.pagesArr[state.pageIndex].t1;
-        goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
+  for (let i = 1; i < 100; i++) {
+    const number = i;
+    const symbolIndex = (i - 1) % symbols.length;
+    const symbol = symbols[symbolIndex];
+    numbersWithSymbols.push({ number, symbol });
 
-        if (state.pagesArr[state.pageIndex].hideNextbtn) {
-            nextbtn.style.display = "none";
-        }
-        else {
-            nextbtn.style.display = "block";
-        }
+    // Store the symbol for multiples of 9
+    if (i % 9 === 0) {
+      symbolMapping[i] = symbol;
     }
+  }
+
+  // Store the symbol mapping for multiples of 9 in state
+  state.symbolMapping = symbolMapping;
+
+  return numbersWithSymbols;
 }
 
-function generateNumbers() {
-    const numbers = [];
-    for (let i = 1; i < 100; i++) {
-        numbers.push(i);
-    }
-    return numbers;
-}
-
-state.pagesArr[4].title = generateNumbers().map(numbers => numbers + " - ").join("<br>");
+// Update the last page to display the symbol associated with the selected number
+state.pagesArr[4].title = generateNumbersWithSymbols()
+  .map((item) => `${item.number} - ${item.symbol}`)
+  .join("<br>");
 
 nextbtn.addEventListener("click", nextPage);
 goReset.addEventListener("click", resetPage);
