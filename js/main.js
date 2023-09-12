ba4f2c939d1c46243f1b495df7e24cad96139945
Parker S <111610335+Psenter@users.noreply.github.com>
2023-09-12 16:43:18 -0400

    App saves users local storage now.

--- js/main.js
+++ js/main.js
@@ -8,36 +8,36 @@ let state = {
   pageIndex: 0,
   pagesArr: [
     { title: "I can read your mind", hideNextbtn: true, t1: "", goReset: "GO" },
-
     {
       title: "Pick a number from 01-99",
       nextbtn: "NEXT",
       t1: "when you have your number click next",
       goReset: "↻",
     },
-
     {
       title: "Add both digits together  to get a new number",
       nextbtn: "NEXT",
       t1: "Ex: 14 is 1 + 4 = 5 <br> click next to proceed",
       goReset: "↻",
     },
-
     {
       title: "Subtract your new number from the original number",
       nextbtn: "NEXT",
       t1: "Ex: 14 - 5 = 9 <br> click next to proceed",
       goReset: "↻",
     },
-
     {
       title: "",
       nextbtn: "REVEAL",
       t1: "Find your new number <br> Note the symbol beside the number",
       goReset: "↻",
     },
-
-    { title: "$", hideNextbtn: true, t1: "Your symbol is: <br> $", goReset: "↻" },
+    {
+      title: "$",
+      hideNextbtn: true,
+      t1: "Your symbol is: <br> $",
+      goReset: "↻",
+    },
   ],
 };
 
@@ -54,14 +54,24 @@ function init() {
   }
 }
 
+function savePageIndexToLocalStorage() {
+  localStorage.setItem("pageIndex", state.pageIndex);
+}
+
+function loadPageIndexFromLocalStorage() {
+  const pageIndex = localStorage.getItem("pageIndex");
+  if (pageIndex !== null) {
+    state.pageIndex = parseInt(pageIndex);
+  }
+}
+
 function nextPage() {
   state.pageIndex = state.pageIndex + 1;
-
+  savePageIndexToLocalStorage();
   title.innerHTML = state.pagesArr[state.pageIndex].title;
   nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
   t1.innerHTML = state.pagesArr[state.pageIndex].t1;
   goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
-
   if (state.pagesArr[state.pageIndex].hideNextbtn) {
     nextbtn.style.display = "none";
   } else {
@@ -72,14 +82,14 @@ function nextPage() {
 function resetPage() {
   if (goReset.innerHTML === "GO") {
     nextPage();
+    savePageIndexToLocalStorage();
   } else if (goReset.innerHTML === "↻") {
     state.pageIndex = 0;
-
+    savePageIndexToLocalStorage();
     title.innerHTML = state.pagesArr[state.pageIndex].title;
     nextbtn.innerHTML = state.pagesArr[state.pageIndex].nextbtn;
     t1.innerHTML = state.pagesArr[state.pageIndex].t1;
     goReset.innerHTML = state.pagesArr[state.pageIndex].goReset;
-
     if (state.pagesArr[state.pageIndex].hideNextbtn) {
       nextbtn.style.display = "none";
     } else {
@@ -98,19 +108,16 @@ function generateNumbersWithSymbols() {
     const symbol = symbols[symbolIndex];
     numbersWithSymbols.push({ number, symbol });
 
-    // Store the symbol for multiples of 9
     if (i % 9 === 0) {
       symbolMapping[i] = symbol;
     }
   }
 
-  // Store the symbol mapping for multiples of 9 in state
   state.symbolMapping = symbolMapping;
 
   return numbersWithSymbols;
 }
 
-// Update the last page to display the symbol associated with the selected number
 state.pagesArr[4].title = generateNumbersWithSymbols()
   .map((item) => `${item.number} - ${item.symbol}`)
   .join("<br>");
@@ -118,4 +125,7 @@ state.pagesArr[4].title = generateNumbersWithSymbols()
 nextbtn.addEventListener("click", nextPage);
 goReset.addEventListener("click", resetPage);
 
-init();
+window.addEventListener("load", function () {
+  loadPageIndexFromLocalStorage();
+  init();
+});
